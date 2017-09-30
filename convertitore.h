53b0b1afc75f1e224ad7127cd99a979052053d0a
Carmine Zaccagnino <carmine@pc-carmine.localdomain>
2017-09-30 15:25:41 +0200

    Aggiunta include guard https://it.wikipedia.org/wiki/Include_guard

--- convertitore.h
+++ convertitore.h
@@ -1,3 +1,5 @@
+#ifndef CONVERTITORE_H          // Include guard:https://it.wikipedia.org/wiki/Include_guard
+#define CONVERTITORE_H
 #include <stdio.h>
 extern int numeroin;            // decobin è il numero che viene dato dall'utente alla prima domanda (se è uguale a
 extern int numeroout;           // 2 significa che l'utente vuole convertire da binario a decimale, se è uguale a 10 il contrario)
@@ -5,3 +7,4 @@ extern int decobin;             // numeroin è il numero che l'utente dà in inp
 void conversione();   // conversione() dovrebbe essere l'unica funzione da chiamare in main() per convertire il numero
 int tobin(int numerodec);       // tobin converte in binario il numero dato in input
 int todec(int numerobin);       // todec fa il contrario
+#endif
