f593022ca1f11a83c5e0ac73c2704189ab01756a
Carmine Zaccagnino <carmine@pc-carmine.localdomain>
2017-09-30 17:32:54 +0200

    Eliminato header inutile

--- tobin.c
+++ tobin.c
@@ -1,6 +1,6 @@
-#include "tobin.h"
-resti=0;
-numeri=0;
+#include "convertitore.h"
+int resti=0;
+int numeri=0;
 int contatore=1;
 int resto=0;
 int tobin(int numerodec)
@@ -10,12 +10,12 @@ int tobin(int numerodec)
     {
         if(numerocorrente%2==0)
         {
-        	resto=0
+        	resto=0;
             resti=resti+resto*contatore;
         }
         else
         {
-        	resto=1
+        	resto=1;
             resti=resti+resto*contatore;
             resti++;
         }
--- tobin.h
+++
@@ -1,6 +0,0 @@
-#ifndef TOBIN_H         // Include guard:https://it.wikipedia.org/wiki/Include_guard
-#define TOBIN_H
-#include "convertitore.h"
-int resti;
-int numeri;
-#endif
