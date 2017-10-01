1e88d017c218d15a4c8e413e5d8e1c915e724268
Carmine Zaccagnino <carmine@pc-carmine.localdomain>
2017-10-01 09:50:52 +0200

    Aggiunto controllo per numero binario, migliorato output

--- todec.c
+++ todec.c
@@ -1,8 +1,11 @@
 #include "convertitore.h"
 int numerodauscire=0;
 int i=0;
+int i2=0;
+bool check_if_bin(int numerodacon);
 int todec(int numerobin)
 {
+  check_if_bin(numerobin);
   numerocorrente=numerobin;
   while(numerocorrente>0)
   {
@@ -11,4 +14,24 @@ int todec(int numerobin)
     i++;
   }
   return numerodauscire;
+ }
+
+bool check_if_bin(int numerodacon)
+{
+  if(numerodacon==0)
+  {
+    return 1;
+  }
+  else
+  {
+    while(numerodacon>0)
+    {
+      if(numerodacon%10!=0 && numerodacon%10!=1)
+        STOP=1;
+      numerodacon=numerodacon/10;
+    }
+    if(STOP)
+    return 0;
+
+  }
 }
